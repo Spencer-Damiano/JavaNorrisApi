7ff332e259bc4adbe927e781bcc7f06e87e6cb55
Spencer Damiano <86030230+Spencer-Damiano@users.noreply.github.com>
2025-03-05 06:50:57 -0700

    Added Github Repo

--- src/main/java/com/spencer/ApiCall.java
+++ src/main/java/com/spencer/ApiCall.java
@@ -1,4 +1,50 @@
 package com.spencer;
 
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
+import java.io.IOException;
+
 public class ApiCall {
+    /*
+     * Table: Summary of Key Components
+     * | Component                  | Description                                      | Example Usage                          |
+     * |----------------------------|--------------------------------------------------|----------------------------------------|
+     * | CloseableHttpClient        | Manages HTTP connections                        | HttpClients.createDefault()            |
+     * | HttpGet                    | Represents GET request                          | new HttpGet("http://targethost")       |
+     * | execute                    | Sends request and gets response                 | httpclient.execute(httpGet)            |
+     * | getStatusLine              | Gets HTTP status line                           | response1.getStatusLine()              |
+     * | getEntity                  | Gets response body                              | response1.getEntity()                  |
+     * | EntityUtils.toString       | Converts entity to string                       | EntityUtils.toString(entity1)          |
+     * | EntityUtils.consume        | Releases entity resources                       | EntityUtils.consume(entity1)           |
+     * | close                      | Closes response to free resources               | response1.close()                      |
+     */
+    public static void main(String[] args) throws IOException {
+        getCategories();
+    }
+
+    public static void getCategories() throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault() ){
+            // 'CloseableHttpClient' used without 'try'-with-resources statement
+
+            // Chuck Norris Joke Categories
+            HttpGet httpGet = new HttpGet("https://api.chucknorris.io/jokes/categories");
+
+            try (ClassicHttpResponse response = httpClient.execute(httpGet)) { // warning on this line: `'execute(org.apache.hc.core5.http.ClassicHttpRequest)' is deprecated`
+
+                System.out.println(response.getCode() + " " + response.getReasonPhrase());
+
+                String responseBody = EntityUtils.toString(response.getEntity());
+                System.out.println(responseBody);
+
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
--- src/main/java/com/spencer/App.java
+++ src/main/java/com/spencer/App.java
@@ -1,6 +1,7 @@
 package com.spencer;
 
 import org.json.JSONObject;
+import java.io.IOException;
 
 /**
  * Hello world!
@@ -8,13 +9,16 @@ import org.json.JSONObject;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
         System.out.println( "Hello World!" );
         JSONObject jsFirst = new JSONObject();
         jsFirst.put("firstName", "John");
         jsFirst.put("lastName", "Doe");
         jsFirst.put("email", "john.doe@gmail.com");
         System.out.println( jsFirst.toString() );
+
+        ApiCall apiCall = new ApiCall();
+
+        ApiCall.getCategories();
     }
 }
