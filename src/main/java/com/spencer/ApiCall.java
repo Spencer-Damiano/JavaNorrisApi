632af8e9c45ebf335a1029d6168c13fc01366d77
= <86030230+Spencer-Damiano@users.noreply.github.com>
2025-03-06 09:05:13 -0700

    Finished
    Added API functionality for categories, random joke, and joke by category. Also added basic CLI user interface. I was not able to figure out the query api function as I thought you would be able to pull the joke ID, not true.

--- src/main/java/com/spencer/ApiCall.java
+++ src/main/java/com/spencer/ApiCall.java
@@ -7,8 +7,12 @@ import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ApiCall {
     /*
@@ -24,12 +28,19 @@ public class ApiCall {
      * | EntityUtils.consume        | Releases entity resources                       | EntityUtils.consume(entity1)           |
      * | close                      | Closes response to free resources               | response1.close()                      |
      */
+
+    public static Map<Integer, String> categoryMap = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         getCategories();
+        getJoke();
+        getJoke(1);
+//        getQuery("9ovbd5b1t66_x92jwrq1yq"); // This was to try and make the query call but I'm not sure how it works.
+//        getQuery("ufgjcp0_qrugwggnmqhvdq"); // I'm not going to worry about it right now cause I don't think it's that helpful
     }
 
     public static void getCategories() throws IOException {
-        try (CloseableHttpClient httpClient = HttpClients.createDefault() ){
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             // 'CloseableHttpClient' used without 'try'-with-resources statement
 
             // Chuck Norris Joke Categories
@@ -37,14 +48,89 @@ public class ApiCall {
 
             try (ClassicHttpResponse response = httpClient.execute(httpGet)) { // warning on this line: `'execute(org.apache.hc.core5.http.ClassicHttpRequest)' is deprecated`
 
-                System.out.println(response.getCode() + " " + response.getReasonPhrase());
+                if (response.getCode() != 200) {
+                    System.out.println(response.getCode() + " " + response.getReasonPhrase());
+                }
 
                 String responseBody = EntityUtils.toString(response.getEntity());
-                System.out.println(responseBody);
+//                System.out.println(responseBody);
 
+                if (categoryMap.isEmpty()) {
+                    JSONArray jsonArray = new JSONArray(responseBody);
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        categoryMap.put(i + 1, jsonArray.getString(i));
+                    }
+                }
+
+                for (Map.Entry<Integer, String> entry : categoryMap.entrySet()) {
+                    System.out.println(entry.getKey() + " " + entry.getValue());
+                }
+
+                System.out.println("0 RETURN TO MENU");
             } catch (ParseException e) {
                 throw new RuntimeException(e);
             }
+        }
+    }
+
+    public static void getJoke() throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet("https://api.chucknorris.io/jokes/random");
+
+            try (ClassicHttpResponse response = httpClient.execute(httpGet)) {
+                printJoke(response);
+            }
+        }
+    }
+
+    public static void getJoke(int categoryIndex) throws IOException {
+        if (!categoryMap.containsKey(categoryIndex)) {
+            System.out.println("Invalid category index: " + categoryIndex);
+            return;
+        }
+
+        String category = categoryMap.get(categoryIndex);
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet("https://api.chucknorris.io/jokes/random?category=" + category);
+
+            try (ClassicHttpResponse response = httpClient.execute(httpGet)) {
+                printJoke(response);
+            }
+        }
+    }
+
+    public static void getQuery (String query) throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet("https://api.chucknorris.io/jokes/search?query=" + query);
+
+            try (ClassicHttpResponse response = httpClient.execute(httpGet)) {
+                printJoke(response);
+            }
+        }
+    }
+
+    public static void printJoke(ClassicHttpResponse response) throws IOException {
+        try {
+            if (response.getCode() != 200) {
+                System.out.println(response.getCode() + " " + response.getReasonPhrase());
+            }
+            String responseBody = EntityUtils.toString(response.getEntity());
+            JSONObject jokeJson = new JSONObject(responseBody);
+//                System.out.println(responseBody);
+
+            if (jokeJson.has("value")) {
+                String jokeStr = jokeJson.getString("value");
+                System.out.println(jokeStr);
+
+//                String query = jokeJson.getString("id"); // Come back to figure out how to query
+//                System.out.println(query);
+            } else {
+                System.out.println("json isn't working as expected");
+            }
+        } catch (ParseException e) {
+            System.out.println("Error printing response: " + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/com/spencer/App.java
+++ src/main/java/com/spencer/App.java
@@ -1,24 +1,61 @@
 package com.spencer;
 
-import org.json.JSONObject;
 import java.io.IOException;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+public class App
 {
     public static void main( String[] args ) throws IOException {
-        System.out.println( "Hello World!" );
-        JSONObject jsFirst = new JSONObject();
-        jsFirst.put("firstName", "John");
-        jsFirst.put("lastName", "Doe");
-        jsFirst.put("email", "john.doe@gmail.com");
-        System.out.println( jsFirst.toString() );
 
-        ApiCall apiCall = new ApiCall();
+        System.out.println("Chuck Norris API Call App");
+        System.out.println("These are true facts about Chuck Norris");
 
-        ApiCall.getCategories();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean running = true;
+        while (running) {
+            printMenu();
+            String input = scanner.nextLine();
+
+            // Skip empty inputs
+            if (input.isEmpty()) {
+                continue;
+            }
+
+            // Check for exit command
+            if (input.equals("exit")) {
+                running = false;
+                break;
+            }
+
+            switch (input) {
+                case "0" -> ApiCall.getCategories();
+                case "1" -> ApiCall.getJoke();
+                case "2" -> {
+                    ApiCall.getCategories();
+                    System.out.println("Pick a Category");
+                    String category = scanner.nextLine();
+                    if (category.equals("0")) { continue; }
+                    ApiCall.getJoke(Integer.parseInt(category));
+                }
+                case "3", "quit" -> {
+                    running = false;
+                    break;
+                }
+                default -> System.out.println("Invalid option. Please try again.");
+            }
+        }
+
+        System.out.println("end of test");
+        scanner.close();
+    }
+
+    public static void printMenu() {
+        System.out.println(" === MENU ===" +
+                "\n 0 - show available joke categories" +
+                "\n 1 - give random joke" +
+                "\n 2 - get joke by category" +
+                "\n 3 or 'quit' - end program" );
     }
 }
